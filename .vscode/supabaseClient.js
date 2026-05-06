e5c671e20d6a2ddacf20f89b9af49955e46b8117
yeimarmendez <yeimar.mendez@estudiantesunibague.edu.co>
2026-05-06 03:57:42 -0500

    Add supabase client

---
+++ .vscode/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "COLE_AQUI_A_URL_DO_PROJETO";
+const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
