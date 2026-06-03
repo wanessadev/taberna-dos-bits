f336c7fe88530993d933038d2105471ee54d755f
Luis Gustavo <luisgustavobl10@gmail.com>
2026-06-03 14:30:29 -0300

    Fix supabase client initialization
    teste

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://nchyjywjupsafqyiesrg.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_bMwb02DdNnFwRD2D85K7mw_5BHHNO3r";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
